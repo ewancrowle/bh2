30294b5bad872d84806aeda0822da8603170977b
Nick <nick@nickcrd.dev>
2022-09-04 01:40:14 +0200

    different components for states

---
+++ components/game/GameRound.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { Game } from "../../libs/gameState";
+import { useReadChannelState } from "@onehop/react";
+
+export const GameContent: React.FC<{
+  game: Game;
+}> = ({ game }, children) => {
+  return (
+    <div>
+      ROUND <br></br>
+      {JSON.stringify(game)}
+    </div>
+  );
+};
